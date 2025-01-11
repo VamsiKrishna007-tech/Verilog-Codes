71323a9a039f3d5f09828d00acff4639faaf36e2
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2025-01-11 15:08:06 +0530

    Update mux_4_to_1.v

--- Full adder using 4:1 MUX /mux_4_to_1.v
+++ Full adder using 4:1 MUX /mux_4_to_1.v
@@ -3,4 +3,10 @@ output y);
 input [3:0]a;
 input [1:0]s;
 assign y=((~s[1])&(~s[0])&a[0])||((~s[1])&(s[0])&a[1])||((s[1])&(~s[0])&a[2])||
-((s[1])&(s[0
+((s[1])&(s[0])&a[3]);
+endmodule
+module full_adder(input a,b,c,
+output sum,carry);
+mux_4_to_1 M1(c,(~c),(~c),c,a,b,sum);
+mux_4_to_1 M2(0,c,c,1,a,b,carry);
+endmodule
