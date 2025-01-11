1e90fc4767491269f9bfdec69a370944b150dc68
Vamsi Krishna <electricals.vamsikrishna@gmail.com>
2025-01-11 16:35:50 +0530

    Update mux_4_to_1.v

--- Full Subtractor using 4:1 MUX /mux_4_to_1.v
+++ Full Subtractor using 4:1 MUX /mux_4_to_1.v
@@ -5,8 +5,3 @@ input [1:0]s;
 assign y=((~s[1])&(~s[0])&a[0])||((~s[1])&(s[0])&a[1])||((s[1])&(~s[0])&a[2])||
 ((s[1])&(s[0])&a[3]);
 endmodule
-module full_sub(input a,b,bin,
-output Borrow,Diff);
-mux_4_to_1 M1(0,(~bin),(~bin),1,a,b,Borrow);
-mux_4_to_1 M2(bin,(~bin),(~bin),bin,a,b,Diff);
-endmodule
